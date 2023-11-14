c9a5e6b2d5c1a66189ae0d88cf048dddade97560
Tyrese <tyresejin3915@gmail.com>
2023-11-14 22:23:44 +0800

    fix

--- src/components/accountDetail.js
+++ src/components/accountDetail.js
@@ -72,6 +72,7 @@ export const AccountDetailComponent = (props) => {
         display: "flex",
         flexDirection: "column",
         position: "relative",
+        overflowY: "auto",
       }}
     >
       {/* <Box
